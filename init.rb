ff13e61240ad1a839297370da3a22d2df8a36668
swiatkiewicz <m.swiatkiewicz91@gmail.com>
2014-09-17 13:34:51 +0200

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_resources_managment do
+  name 'Redmine Resources Managment plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
