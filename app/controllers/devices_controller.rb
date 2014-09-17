160e22d646f63b1b8f7c5754c2e29b59ad1231ac
swiatkiewicz <m.swiatkiewicz91@gmail.com>
2014-09-17 14:33:51 +0200

    initial commit

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,5 @@
+class DevicesController < ApplicationController
+  unloadable
+
+
+end
---
+++ db/migrate/001_create_devices.rb
@@ -0,0 +1,9 @@
+class CreateDevices < ActiveRecord::Migration
+  def change
+    create_table :devices do |t|
+
+
+    end
+
+  end
+end
