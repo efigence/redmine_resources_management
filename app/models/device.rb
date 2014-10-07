4e9dd860423ed54a272d389c5536c0031b9a4289
swiatkiewicz <m.swiatkiewicz91@gmail.com>
2014-10-07 12:08:48 +0200

    fixed all errors and refactor

--- app/models/device.rb
+++ app/models/device.rb
@@ -9,38 +9,34 @@ class Device < ActiveRecord::Base
   has_many :loans, dependent: :destroy
   has_attached_file :picture, :styles => { thumb: '100x100>', medium: '200x200' }, default_url: '/images/:style/missing.png'
 
-  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
-  validates_length_of :name, :owner, {:maximum => 200}
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+  validates_length_of :name, :owner, { maximum: 200 }
   validates_presence_of :name, :owner
-  validate :valid_date_to
+  validate :valid_date_to, if: :date_to && :date_from
 
   def set_default_status
-    if self.date_from && self.date_from > Date.today
+    if date_from && date_from > Date.today
       self.status = Loan::STATUS[:not_yet]
     else
       self.status = Loan::STATUS[:available]
     end
-    self.save
+    save
   end
 
   def valid_date_to
-    if date_to
-      if date_to < date_from
-        errors.add :date_to, :date_to_greater_then_dato_from
-      end
-    end
+    errors.add :date_to, :date_to_greater_then_dato_from if date_to < date_from
   end
 
   def change_status!
-    user = User.find(self.loans.last.borrower_id)
+    user = User.find(loans.last.borrower_id)
     zone = user.time_zone || Time.zone
     self.status = Loan::STATUS[:unavailable]
-    self.loan_date = self.loans.last.date_of_return.in_time_zone(zone)
-    self.loans_name = self.loans.last.borrower_id
-    self.save
+    self.loan_date = loans.last.date_of_return.in_time_zone(zone)
+    self.loans_name = loans.last.borrower_id
+    save
   end
 
-  def self.search search
+  def self.search(search)
     if search
       where('name LIKE ?', "%#{search}%")
     else
@@ -50,11 +46,11 @@ class Device < ActiveRecord::Base
 
   def enforce_time_report_worker
     reload
-    user_id = self.loans.last.borrower_id
-    run_worker(user_id)
+    borrower = loans.last.borrower_id if loans.any?
+    run_worker(borrower) unless borrower.blank?
   end
 
   def run_worker(user_id)
-    Redmine::Hook.call_hook(:run_worker_for_device_user, {:user_id => user_id })
+    Redmine::Hook.call_hook(:run_worker_for_device_user, { user_id: user_id })
   end
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,95 +1,84 @@
 class Loan < ActiveRecord::Base
   unloadable
 
-  STATUS = { :available => 'AVAILABLE', :unavailable => 'UNAVAILABLE', :not_yet => 'UNAVAILABLE YET' }
+  STATUS = { available: 'AVAILABLE', unavailable: 'UNAVAILABLE', not_yet: 'UNAVAILABLE YET' }
 
   attr_accessible :device_id, :date_of_hire, :date_of_return, :status, :phone,
     :email_notify, :phone_notify, :borrower_id, :phone_time, :email_time
+
   belongs_to :device
-  before_create :check_values_without_notify_method
+
+  before_create :set_values_without_notify
   after_create :prepare_to_send_notification
 
   validates_presence_of :borrower_id, :date_of_hire, :date_of_return, :status
 
-  validates_numericality_of :phone, :if => :phone_notify
-  validates_format_of :phone, with: /\A\+\d{11}\z/i, message: "should be +48 first", :allow_blank => true, :allow_nil => true, :if => :phone_notify
-  validates_numericality_of :phone_time, :allow_nil => false, :allow_blank => false, :if => :phone_notify
+  validates_format_of :phone, with: /\A\+\d{11}\z/i, message: :wrong_format,
+    allow_blank: true, allow_nil: true, if: :phone_notify
 
-  validates_numericality_of :email_time, :allow_nil => false, :allow_blank => false, :if => :email_notify
+  validates_numericality_of :phone, :phone_time, allow_nil: false, allow_blank: false, if: :phone_notify
+  validates_numericality_of :email_time, allow_nil: false, allow_blank: false, if: :email_notify
 
-  validate :date_of_return_higher
-  validate :time_of_hire
-
+  validate :time_of_hire, :date_of_return_higher, if: :date_of_return
 
   def return!
-    dev = Device.find(self.device_id)
-    dev.status = STATUS[:available]
-    dev.loan_date = nil
-    dev.loans_name = nil
+    device.status = STATUS[:available]
+    device.loan_date, device.loans_name = nil, nil
     self.status = STATUS[:available]
     self.date_of_return = Time.now
-    check_for_notifications self.id, self.borrower_id
-    dev.save
-    self.save
+    check_for_notifications id, borrower_id
+    device.save
+    save
   end
 
   def prepare_to_send_notification
-    user = User.find(self.borrower_id)
-    notify_by_email(user) if self.email_notify == true
-    notify_by_sms(user) if self.phone_notify == true
+    user = User.find(borrower_id)
+    notify_by_email(user) if email_notify == true
+    notify_by_sms(user) if phone_notify == true
   end
 
-  def check_values_without_notify_method
-    self.phone = nil if !self.phone_notify
-    self.phone_time = nil if !self.phone_notify
-    self.email_time = nil if !self.email_notify
+  def set_values_without_notify
+    self.phone = nil unless phone_notify
+    self.phone_time = nil unless phone_notify
+    self.email_time = nil unless email_notify
   end
 
-  def notify_by_email user
+  def notify_by_email(user)
     notification = Notification.new
-    notification.notifiable_id = self.id
-    notification.notifiable_type = "Device"
-    notification.planning_send_at = (date_of_return - self.email_time.hours)
+    notification.notifiable_id = id
+    notification.notifiable_type = 'Device'
+    notification.planning_send_at = (date_of_return - email_time.hours)
     notification.to = user.mail
     notification.kind = :mail
     notification.user_id = user.id
-    notification.subject = "#{Device.find(self.device_id).name} " + "should be return for" + " #{self.email_time} h."
-    notification.content = "#{Device.find(self.device_id).name} should be returnder today: #{Date.today} about #{self.email_time} h.!!"
+    notification.subject = "#{device.name} " + 'should be return for' + " #{email_time} h."
+    notification.content = "#{device.name} should be returned at #{date_of_return} otherwise your internet will be blocked!"
     notification.save
   end
 
-  def notify_by_sms user
+  def notify_by_sms(user)
     notification = Notification.new
-    notification.notifiable_id = self.id
-    notification.notifiable_type = "Device"
-    notification.planning_send_at = (date_of_return - self.phone_time.hours)
-    notification.to = self.phone
+    notification.notifiable_id = id
+    notification.notifiable_type = 'Device'
+    notification.planning_send_at = (date_of_return - phone_time.hours)
+    notification.to = phone
     notification.kind = :sms
     notification.user_id = user.id
-    notification.content = "#{Device.find(self.device_id).name} should be return for #{self.email_time} h."
+    notification.content = "#{device.name} should be return for #{email_time} h."
     notification.save
   end
 
-  def check_for_notifications loan_id, user_id
-    Notification.where('user_id = ? AND notifiable_id = ?', user_id, loan_id).destroy_all
+  def check_for_notifications(loan_id, user_id)
+    Notification.where('user_id = ? AND notifiable_id = ? AND sent_at IS NULL', user_id, loan_id).destroy_all
   end
 
   def date_of_return_higher
-    unless !date_of_return
-      if date_of_hire > date_of_return
-        errors.add :date_of_return, :greater_then_date_of_hire
-      end
-    end
+    errors.add :date_of_return, :greater_then_date_of_hire if date_of_hire > date_of_return
   end
 
   def time_of_hire
-    dev = Device.find(self.device_id)
-    unless dev.date_to.blank?
-      unless !date_of_return
-        if dev.date_to < self.date_of_return.to_date
-          errors.add :date_of_return, :less_then_avalible_date
-        end
-      end
+    unless device.date_to.blank?
+      errors.add :date_of_return, :less_then_avalible_date if device.date_to < date_of_return.to_date
     end
   end
 end
